7e7659ae643f54fddc6605d10d73edebc2ecfda8
Vedant Dalvi <vedantdalvi@Vedants-MacBook-Air.local>
2022-05-10 18:27:27 +0530

    ERC777 token contract added

---
+++ contracts/Interfaces/Istkbnb.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
+
+interface IStakedBNBToken is IERC777 {
+    function mint(
+        address account,
+        uint256 amount,
+        bytes memory userData,
+        bytes memory operatorData
+    ) external;
+}
