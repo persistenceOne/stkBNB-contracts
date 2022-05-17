e74d30eb0d8e88303d137abff9977abf7125ecc8
Vedant Dalvi <vedantdalvi@Vedants-MacBook-Air.local>
2022-05-17 17:18:31 +0530

    changes

--- contracts/Permissions.sol
+++ contracts/Permissions.sol
@@ -1,29 +1,27 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "./Interfaces/IPermissions.sol";
 
-abstract contract Permissions is IPermissions, AccessControl {
+
+abstract contract Permissions is AccessControl {
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant GOVERN_ROLE = keccak256("GOVERN_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
 
     constructor(){
         
-        _setRoleAdmin(MINTER_ROLE, GOVERN_ROLE);
-        _setRoleAdmin(GOVERN_ROLE, GOVERN_ROLE);
-        _setRoleAdmin(BURNER_ROLE, GOVERN_ROLE);
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
 
 
     }
 
-    modifier onlyGovernor() {
+    modifier onlyAdmin() {
         require(
-            isGovernor(msg.sender),
-            "Permissions: Caller is not a governor"
+            isAdmin(msg.sender),
+            "Permissions: Caller is not an Admin"
         );
         _;
     }
@@ -46,21 +44,20 @@ abstract contract Permissions is IPermissions, AccessControl {
     /// @dev can also be used to update admin of existing role
     function createRole(bytes32 role, bytes32 adminRole)
         public
-        override
-        onlyGovernor
+        onlyAdmin
     {
         _setRoleAdmin(role, adminRole);
     }
 
     /// @notice grants minter role to address
     /// @param minter new minter
-    function grantMinter(address minter) public override onlyGovernor {
+    function grantMinter(address minter) public onlyAdmin {
         grantRole(MINTER_ROLE, minter);
     }
 
     /// @notice grants burner role to address
     /// @param burner new burner
-    function grantBurner(address burner) public override onlyGovernor {
+    function grantBurner(address burner) public onlyAdmin {
         grantRole(BURNER_ROLE, burner);
     }
 
@@ -69,19 +66,19 @@ abstract contract Permissions is IPermissions, AccessControl {
 
     /// @notice grants governor role to address
     /// @param governor new governor
-    function grantGovernor(address governor) public override onlyGovernor {
-        grantRole(GOVERN_ROLE, governor);
-    }
+    // function grantGovernor(address governor) public override onlyGovernor {
+    //     grantRole(GOVERN_ROLE, governor);
+    // }
 
     /// @notice revokes minter role from address
     /// @param minter ex minter
-    function revokeMinter(address minter) public override onlyGovernor {
+    function revokeMinter(address minter) public onlyAdmin {
         revokeRole(MINTER_ROLE, minter);
     }
 
     /// @notice revokes burner role from address
     /// @param burner ex burner
-    function revokeBurner(address burner) public override onlyGovernor {
+    function revokeBurner(address burner) public onlyAdmin {
         revokeRole(BURNER_ROLE, burner);
     }
 
@@ -90,40 +87,40 @@ abstract contract Permissions is IPermissions, AccessControl {
 
     /// @notice revokes governor role from address
     /// @param governor ex governor
-    function revokeGovernor(address governor) public override onlyGovernor {
-        revokeRole(GOVERN_ROLE, governor);
-    }
+    // function revokeGovernor(address governor) public override onlyGovernor {
+    //     revokeRole(GOVERN_ROLE, governor);
+    // }
 
     /// @notice checks if address is a minter
     /// @param _address address to check
     /// @return true _address is a minter
-    function isMinter(address _address) public view override returns (bool) {
+    function isMinter(address _address) public view returns (bool) {
         return hasRole(MINTER_ROLE, _address);
     }
 
     /// @notice checks if address is a burner
     /// @param _address address to check
     /// @return true _address is a burner
-    function isBurner(address _address) public view override returns (bool) {
+    function isBurner(address _address) public view returns (bool) {
         return hasRole(BURNER_ROLE, _address);
     }
 
-    /// @notice checks if address is a governor
+    /// @notice checks if address is a Admin
     /// @param _address address to check
-    /// @return true _address is a governor
-    // only virtual for testing mock override
-    function isGovernor(address _address)
+    /// @return true _address is a Admin
+    // // only virtual for testing mock override
+    function isAdmin(address _address)
         public
         view
         virtual
-        override
         returns (bool)
     {
-        return hasRole(GOVERN_ROLE, _address);
+        return hasRole(DEFAULT_ADMIN_ROLE, _address);
     }
 
-    function _setupGovernor(address governor) internal {
-        _setupRole(GOVERN_ROLE, governor);
-    }
+    // function _setupGovernor(address governor) internal {
+    //     _setupRole(GOVERN_ROLE, governor);
+    // }
 
 }
+
