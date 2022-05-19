9916b0f3996c19f46d4f4589fb92c1f3a67d365a
Vedant Dalvi <vedantdalvi@Vedants-MacBook-Air.local>
2022-05-19 13:51:24 +0530

    changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -35,6 +35,7 @@ module.exports = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
+      blockGasLimit:40000000,
       accounts: {mnemonic: mnemonic}
     },
     mainnet: {
