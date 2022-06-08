b3617ebc57d48ec35b9a9943dce2dcf4d86eb104
Abhimanyu Singh Gaur <12651351+abhimanyusinghgaur@users.noreply.github.com>
2022-06-08 20:05:15 +0530

    Add workflows & fixup everything
    - Added workflows for test, lint, contract-size, and abi generation.
    - Fixed the existing slither workflow.
    - Fixed the hardhat setup, added missing dependencies.
    - Fixed the deploy script.
    - Fixed the tests.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  env: {
-    browser: false,
-    es2021: true,
-    mocha: true,
-    node: true,
-  },
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  overrides: [
-    {
-      files: ["hardhat.config.js"],
-      globals: { task: true },
+    env: {
+        browser: false,
+        es2021: true,
+        mocha: true,
+        node: true,
     },
-  ],
+    extends: [
+        'standard',
+        'plugin:prettier/recommended',
+        'plugin:node/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {},
 };
+
---
+++ contracts/StakedBNBToken.sol
@@ -0,0 +1,126 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+/**
+ * @dev {ERC777} token, including:
+ *
+ *  - a minter role that allows for token minting (creation)
+ *  - a burner role that allows for token burning (destruction)
+ *
+ * This contract uses {AccessControl} to lock permissioned functions using the
+ * different roles - head to its documentation for details.
+ *
+ * The account that deploys the contract will be granted the default admin role,
+ * which will let it grant the minter and burner roles to the StakePool contract.
+ *
+/**/
+
+/// @custom:security-contact support@persistence.one
+contract StakedBNBToken is ERC777, AccessControlEnumerable, Pausable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor() ERC777("Staked BNB", "stkBNB", new address[](0)) {
+        // Make the deployer the default admin, deployer will later transfer this role to a multi-sig.
+        // Once we are sure the system is stable and there are no issues, the multi-sig can choose to renounce this role.
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC777-_burn}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `BURNER_ROLE`.
+     */
+    function burn(uint256 amount, bytes memory data)
+        public
+        override
+        onlyRole(BURNER_ROLE)
+        whenNotPaused
+    {
+        ERC777.burn(amount, data);
+    }
+
+    /**
+     * @dev See {IERC777-operatorBurn}.
+     *
+     * Emits {Burned} and {IERC20-Transfer} events.
+     * Overridden to ensure that only the address with the BURNER_ROLE can call this.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `BURNER_ROLE`.
+     */
+    function operatorBurn(
+        address account,
+        uint256 amount,
+        bytes memory data,
+        bytes memory operatorData
+    ) public override onlyRole(BURNER_ROLE) whenNotPaused {
+        ERC777.operatorBurn(account, amount, data, operatorData);
+    }
+
+    /**
+     * @dev Creates `amount` new tokens for `account`.
+     *
+     * See {ERC777-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(
+        address account,
+        uint256 amount,
+        bytes memory userData,
+        bytes memory operatorData
+    ) public onlyRole(MINTER_ROLE) whenNotPaused {
+        ERC777._mint(account, amount, userData, operatorData);
+    }
+
+    /**
+     * @dev selfDestruct
+     *
+     * The contract will be destroyed and BNB (if any) will be sent to the provided address.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `DEFAULT_ADMIN_ROLE`.
+     *
+     */
+    function selfDestruct(address _address) public onlyRole(DEFAULT_ADMIN_ROLE) whenPaused {
+        selfdestruct(payable(_address));
+    }
+
+    /**
+     * @dev pause: Used by admin to pause the contract.
+     *             Supposed to be used in case of a prod disaster.
+     *
+     * Requirements:
+     *
+     * - The caller must have the DEFAULT_ADMIN_ROLE.
+     */
+    function pause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _pause();
+    }
+
+    /**
+     * @dev unpause: Used by admin to resume the contract.
+     *               Supposed to be used after the prod disaster has been mitigated successfully.
+     *
+     * Requirements:
+     *
+     * - The caller must have the DEFAULT_ADMIN_ROLE.
+     */
+    function unpause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _unpause();
+    }
+}
