2499c12413bd7ce6e640277514a50f5a7216625a
Gad Elbaz <gad@certora.com>
2022-07-12 16:11:15 +0300

    add link to stkBNB

--- certora/scripts/run.sh
+++ certora/scripts/run.sh
@@ -1,5 +1,5 @@
 certoraRun  certora/harness/StakePoolHarness.sol contracts/StakedBNBToken.sol contracts/AddressStore.sol \
---link StakePoolHarness:addressStore=AddressStore \
+--link StakePoolHarness:addressStore=AddressStore StakePoolHarness:stkBNB=StakedBNBToken \
 --verify StakePoolHarness:certora/specs/StakePool.spec \
 --solc solc8.7 \
 --msg "StakePoolHarness integrityOfDeposit"  \
