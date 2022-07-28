788288c03d073c7a252cafcdd623386095c05c4e
hristo <hristo@certora.com>
2022-07-28 18:58:32 +0300

    added explainer to the run.sh

--- certora/scripts/run.sh
+++ certora/scripts/run.sh
@@ -1,3 +1,35 @@
+# The goal of this script is the help run the tool
+# without having to enter manually all the required
+# parameters every time a test is executed
+#
+# The script should be executed from the terminal,
+# with the project folder as the working folder
+#
+#
+# The script can be run either with:
+#
+# 1) no parameters --> all the rules in the .spec file are tested
+#    example:
+#
+#    ./certora/scripts/run.sh
+# 
+#
+# 2) with one parameter only --> the parameter states the rule name
+#    example, when the rule name is "integrityOfDeposit":
+#
+#    ./certora/scripts/run.sh integrityOfDeposit
+#
+#
+# 3) with two parameters:
+#     - the first parameter is the rule name, as in 2)
+#     - the second parameter is an optional message to help distinguish the rule
+#       the second parameter should be encircled "with quotes"
+#    example:
+#
+#    ./certora/scripts/run.sh integrityOfDeposit "user should get stkBNB for any deposit"
+
+
+
 if [[ "$1" ]]
 then
     RULE="--rule $1"
@@ -18,14 +50,13 @@ certoraRun  certora/harness/StakePoolHarness.sol \
 --packages @openzeppelin=node_modules/@openzeppelin \
 --path . \
 --solc solc8.7 \
---staging \
 --loop_iter 3 \
 --settings -optimisticFallback=true --optimistic_loop \
 $RULE  \
 --msg "pstake -$RULE $MSG"
 
-#        StakePoolHarness:config=Config \
-#        StakePoolHarness:exchangeRate=ExchangeRate \
-#        StakePoolHarness:BasisFee=BasisFee \
+
+# additional parameters that might be helpful:
 #--optimistic_loop
 #--settings -optimisticFallback=true \
+#--staging \
