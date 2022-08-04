362a7a833395be7a0846ddf474ddfd0b4be69fe8
Vedant Dalvi <vedantdalvi@Vedants-MacBook-Air.local>
2022-08-04 17:21:17 +0400

    add bot test case

---
+++ agents/large-value-events/src/agent.spec.ts
@@ -0,0 +1,44 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent,
+  ethers,
+} from "forta-agent";
+import agent, {
+  STAKE_POOL_DEPOSIT_EVENT,
+  STAKEPOOL_ADDRESS,
+  STAKE_POOL_WITHDRAW_EVENT,
+  STKBNB_MINT_EVENT,
+  STKBNB_BURN_EVENT,
+  STAKEDBNBTOKEN_ADDRESS
+  
+} from "./agent";
+
+describe("Large value stkBNB deposit agent", () => {
+  let handleTransaction: HandleTransaction;
+  const mockTxEvent = createTransactionEvent({} as any);
+
+  beforeAll(() => {
+    handleTransaction = agent.handleTransaction;
+  });
+
+  describe("handleTransaction", () => {
+    it("returns empty findings if there are no stkBNB ", async () => {
+      mockTxEvent.filterLog = jest.fn().mockReturnValue([]);
+
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(4);
+
+    });
+   
+  });
+});
+
+
+
+
+
