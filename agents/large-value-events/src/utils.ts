6d777abbb06c667b5ae2cb6c3eb27e6124caecf3
Vedant Dalvi <vedantdalvi20@gmail.com>
2022-08-04 17:14:14 +0400

    Utils changes

--- agents/large-value-events/src/utils.ts
+++ agents/large-value-events/src/utils.ts
@@ -1,11 +1,7 @@
 import { BigNumber } from 'ethers';
 const { ethers } = require('ethers');
 
-// export function normalizeValue(value){
-//     return value.div(ethers.constants.WeiPerEther)
-// }
-
-export function normalizedValue(value: any) {
-    value = value / ethers.constants.WeiPerEther;
+export function normalizedValue(value: BigNumber) {
+    value = value.div(ethers.constants.WeiPerEther);
     return value;
 }
