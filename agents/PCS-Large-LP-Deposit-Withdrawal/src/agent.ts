3208341ea3e85aae9405d2c91c9cc393ddf58388
Vedant Dalvi <vedantdalvi20@gmail.com>
2022-10-10 16:36:32 +0400

    PR fixes

--- agents/PCS-Large-LP-Deposit-Withdrawal/src/agent.ts
+++ agents/PCS-Large-LP-Deposit-Withdrawal/src/agent.ts
@@ -5,6 +5,8 @@ import { createPair } from "./utils";
 import { createFinding } from "./finding";
 import PoolFetcher from "./pool.fetcher";
 const STKBNB_POOL_ADDRESS = "0xaa2527ff1893e0d40d4a454623d362b79e8bb7f1";
+const STKBNB_TOKEN_ADDRESS = "0xc2E9d07F66A89c44062459A47a0D2Dc038E4fb16";
+const WBNB_TOKEN_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 
 export const provideHandleTransaction =
   (
@@ -22,16 +24,20 @@ export const provideHandleTransaction =
     await Promise.all(
       logs.map(async (log) => {
         const { amount0, amount1 } = log.args;
-        const [valid, token0, token1, totalSupply] = await fetcher.getPoolData(block - 1, log.address);
-        const createdPair = createPair(token0, token1, factory);
+        const [valid,totalSupply] = await fetcher.getPoolData(block - 1, log.address);
+        
+        const createdPair = createPair( WBNB_TOKEN_ADDRESS, STKBNB_TOKEN_ADDRESS, factory);
         if (valid && log.address === createdPair) {
-          const [balance0, balance1] = await fetcher.getPoolBalance(block - 1, log.address, token0, token1);
+          
+          const [balance0, balance1] = await fetcher.getPoolBalance(block - 1, log.address, WBNB_TOKEN_ADDRESS, STKBNB_TOKEN_ADDRESS);
           if (
+            log.address == STKBNB_POOL_ADDRESS &&
             totalSupply.gt(poolSupplyThreshold) &&
             (amount0.mul(100).gt(balance0.mul(thresholdPercentage)) ||
               amount1.mul(100).gt(balance1.mul(thresholdPercentage)))
           ) {
-            findings.push(createFinding(log, token0, token1, totalSupply));
+            
+            findings.push(createFinding(log,totalSupply));
           }
         }
       })
