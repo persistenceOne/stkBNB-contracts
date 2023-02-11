5281e9203185de7263a2888d47681e908e8c9fb6
Vishal Singh <vishalnysingh@gmail.com>
2023-02-11 14:04:58 +0530

    Requested changes

--- certora/scripts/ci.sh
+++ certora/scripts/ci.sh
@@ -13,3 +13,18 @@ certoraRun  certora/harness/StakePoolHarness.sol \
 --settings -assumeUnwindCond,-enableStorageAnalysis=true,-ciMode=true,-optimisticFallback=true \
 --msg "pstake"
 
+certoraRun  certora/harness/StakedBNBTokenHarness.sol \
+            contracts/AddressStore.sol \
+            contracts/FeeVault.sol \
+            contracts/StakePool.sol \
+            contracts/TimelockedAdmin.sol \
+            contracts/UndelegationHolder.sol  --link  StakedBNBTokenHarness:_addressStore=AddressStore \
+--verify StakedBNBTokenHarness:certora/specs/StkBNB.spec \
+--packages @openzeppelin=node_modules/@openzeppelin \
+--path . \
+--loop_iter 3 \
+--staging master \
+--rule_sanity \
+--settings -assumeUnwindCond,-enableStorageAnalysis=true,-ciMode=true,-optimisticFallback=true \
+--msg "stkBNB"
+
